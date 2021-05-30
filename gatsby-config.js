e1d6d3d3a9cc0cbb99211270040542642750c789
George W Langham (desktop) <georgelangham1990@gmail.com>
2021-05-30 14:52:31 +0100

    change sit title sitemetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 
 module.exports = {
   siteMetadata: {
+    siteTitle: `GeorgeWL`,
     siteTitleAlt: `georgewl.dev - George Langham`,
   },
   plugins: [
