a791eed6df56d2cf4002542d09b057bfde7cc03e
George W Langham (desktop) <georgelangham1990@gmail.com>
2021-12-11 18:51:06 +0000

    remove unused component

--- components/Image.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Image as ImageThemed } from 'theme-ui';
-const Image = ({ src, title, ...props }) => (
-  <ImageThemed src={src} title={title} {...props} />
-);
-
-export default Image;
--- components/ImageContainer.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const ImageContainer = ({ children }) => <div>{children}</div>;
-
-export default ImageContainer;
